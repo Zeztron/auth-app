8fc1712ac05e26c97bd88a62c63960b281de7a0b
Zeztron <pateltharsh@gmail.com>
2022-08-15 12:06:12 -0500

    added product controller and schema

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,26 @@
+import {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+  deleteProductSchema,
+} from './schema/product.schema';
 import { createUserSchema } from './schema/user.schema';
 import { Express, Request, Response } from 'express';
 import { createUserHandler } from './controllers/user.controller';
 import validateResource from './middleware/validateResource';
 import {
   createUserSessionHandler,
+  deleteSessionHandler,
   getUserSessionsHandler,
 } from './controllers/session.controller';
 import { createSessionSchema } from './schema/session.schema';
 import requireUser from './middleware/requireUser';
+import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from './controllers/product.controller';
 
 function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => {
@@ -23,6 +36,32 @@ function routes(app: Express) {
   );
 
   app.get('/api/sessions', requireUser, getUserSessionsHandler);
+
+  app.delete('/api/sessions', requireUser, deleteSessionHandler);
+
+  app.post(
+    '/api/products',
+    [requireUser, validateResource(createProductSchema)],
+    createProductHandler
+  );
+
+  app.put(
+    '/api/products/:productId',
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler
+  );
+
+  app.get(
+    '/api/products/:productId',
+    [requireUser, validateResource(getProductSchema)],
+    getProductHandler
+  );
+
+  app.delete(
+    '/api/products/:productId',
+    [requireUser, validateResource(deleteProductSchema)],
+    deleteProductHandler
+  );
 }
 
 export default routes;
